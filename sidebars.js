a0c580cfc1637b604cdf1fb6975c357673453d20
ningyongheng <ningyongheng@jeejio.com>
2025-01-10 17:23:12 +0800

    feat: 没有解决当前的搜索功能框实现

---
+++ sidebars.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: ningyongheng ningyongheng@jeejio.com
+ * @Date: 2025-01-10 16:09:01
+ * @LastEditors: ningyongheng ningyongheng@jeejio.com
+ * @LastEditTime: 2025-01-10 16:22:09
+ * @FilePath: /my-website/sidebars.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  someSidebar: [
+    "intro",
+    {
+      type: "category",
+      label: "tutorial-basics",
+      items: [
+        "tutorial-basics/congratulations",
+        "tutorial-basics/create-a-blog-post",
+        "tutorial-basics/create-a-document",
+      ],
+    },
+  ],
+};
